09e68adaa403401c0645d0a47183ec2475c1c4b3
kutaybuyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-08-29 17:38:15 +0300

    checking the .v of the project

--- Android/Maps/app/src/main/java/com/scalx/locationly/config/AppParameters.java
+++ Android/Maps/app/src/main/java/com/scalx/locationly/config/AppParameters.java
@@ -3,7 +3,7 @@ package com.scalx.locationly.config;
 import com.scalx.locationly.model.UserType;
 
 public class AppParameters {
-    public static final String REST_API_URL="http://52.143.175.25:8082/";
+    public static final String REST_API_URL="http://localhost:8082/";
     public static boolean doubleBackToExitPressedOnce = false;
     public static UserType appUser = new UserType();
 }
